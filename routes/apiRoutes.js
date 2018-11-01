7ef9d5bb0adaebe1c257ac8b6847b736c490eb2d
Chace Powell <cpcpowell8@gmail.com>
2018-10-31 20:52:54 -0400

    forgot what I did. TODO: start building out api routes.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,9 @@
+const db = require('../models');
+const axios = require('axios');
+
 module.exports = (app) => {
 
+  app.get('/scrape', (req, res) => {
+
+  });
 };
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
+const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.engine('.hbs', exphbs({defaultLayout: 'main'}));
+app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
 app.use(express.static('public'));
