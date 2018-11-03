22fe6f2702aca29093114f0f861789028094d4b0
Chace Powell <cpcpowell8@gmail.com>
2018-11-02 20:37:38 -0400

    add mongoose connect in server

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const logger = require('morgan');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.engine('.hbs', exphbs({defaultLayout: 'main'}));
+app.engine('.hbs', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
 app.use(express.static('public'));
+
+mongoose.connect(
+  'mongodb://localhost/newsdb',
+  { useNewUrlParser: true }
+);
 
 require('./routes/htmlRoutes')(app);
 require('./routes/apiRoutes')(app);
