d133db5fb7877f16d7b60850f721aa9a31577f57
Chace Powell <cpcpowell8@gmail.com>
2018-11-05 20:37:14 -0500

    add comment section

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,7 @@ const createArticles = require('../helpers/createArticle');
 
 module.exports = app => {
   app.get('/home', (req, res) => {
-    db.Article.find()
+    db.Article.find().populate('comment')
       .then(articles => res.render('home', { articles }));
   });
 };
